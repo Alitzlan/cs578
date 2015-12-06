f44b8ced10368902d8aaab3b5fb7e08320f54e6b
Chi Yang <alitzlan@gmail.com>
2015-12-06 16:22:27 -0600

    finish compressing data

--- main.py
+++ main.py
@@ -5,19 +5,60 @@ import itertools
 import pickle
 import numpy as np
 import os.path
+import sys
 from collections import Counter
 from sklearn.decomposition import PCA
 
-def processfile(filename):
-    with open(filename) as datafile:    
+def gettagset(filename):
+    with open(filename,"rb") as datafile:    
         data = json.load(datafile)
         alltags = itertools.chain(*[x["ingredients"] for x in data])
+        tag_set = [x[0] for x in Counter(alltags).most_common()]
+        # print tagset
+        return tag_set
+
+def getfiledata(filename):
+    with open(filename,"rb") as datafile:    
+        data = json.load(datafile)
+        alltags = itertools.chain(*[x["ingredients"] for x in data])
+        tag_set = [x[0] for x in Counter(alltags).most_common()]
+        bin_data = []
+        lbl_data = []
+        print "processing file..."
+        for i,x in enumerate(data):
+            sys.stdout.write("current: {0}/{1}\r".format(i,len(data)))
+            sys.stdout.flush()
+            bin_data.append(np.array([1 if t in x["ingredients"] else 0 for t in tag_set], dtype="bool"))
+            lbl_data.append(x["cuisine"])
+        bin_data = np.array(bin_data, dtype="bool")
+        # print bin_data 
+        # print lbl_data
+        # print tag_set
+        return bin_data, lbl_data, tag_set
+    
+def getfilelbl(filename):
+    with open(filename,"rb") as datafile:    
+        data = json.load(datafile)
         lbl_data = [x["cuisine"] for x in data]
-        tagset_c = Counter(alltags).most_common()
-        tagset = [x[0] for x in tagset_c]
-        bin_data = np.array([np.array([1 if t in x["ingredients"] else 0 for t in tagset]) for x in data])
-        print bin_data
-        return bin_data, lbl_data
+        # print lbl_data
+        return lbl_data
+    
+def loaddata(filename):
+    # load bin_data
+    objfilename = filename + '.dat'
+    if os.path.isfile(objfilename):
+        with open(objfilename,'rb') as objfile:
+            bin_data = np.load(objfile)
+        #load lbl_data
+        lbl_data = getfilelbl(filename)
+        #load tag_set
+        tag_set = gettagset(filename)
+    else:
+        bin_data, lbl_data, tag_set = getfiledata(filename)
+        with open(objfilename,'wb') as objfile:
+            np.save(objfile, bin_data)
+    
+    return bin_data, lbl_data, tag_set
     
 def shuffledata(datalen, numpart):
     idx = range(0, datalen)
@@ -28,27 +69,10 @@ def shuffledata(datalen, numpart):
         parts.append(idx[i*step:(i+1)*step])
     return parts
     
-def readdata(filename):
-    objfilename = filename + '.dat'
-    if os.path.isfile(objfilename):
-        with open(objfilename,'rb') as objfile:
-            bin_data, lbl_data = pickle.load(objfile)
-    else:
-        bin_data, lbl_data = processfile(filename)
-        with open(objfilename,'wb') as objfile:
-            pickle.dump([bin_data, lbl_data], objfile)
-    
 def main():
-    bin_data, lbl_data = readdata('train.json')
+    bin_data, lbl_data, tag_set = loaddata('train.json')
     indices = shuffledata(len(bin_data), 10)
     print 'finish processing data'
-
-    # Added
-    print "Starts doing PCA"
-    num_parameter = len(bin_data[0])
-    pca = PCA(n_components = 2)
-    pca.fit(bin_data)
-    print(pca.explained_variance_ratio_)
     
 if __name__ == "__main__":
     main()

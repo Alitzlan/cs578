ec632b56a68e80d4199feaa947c4a94115006eb8
Chi Yang <alitzlan@gmail.com>
2015-12-05 18:04:43 -0600

    finish data shuffle

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ from collections import Counter
 def processfile(filename):
     with open(filename) as datafile:    
         data = json.load(datafile)
-        alltags = list(itertools.chain([x["ingredients"] for x in data]))
+        alltags = itertools.chain(*[x["ingredients"] for x in data])
         lbl_data = [x["cuisine"] for x in data]
         tagset_c = Counter(alltags).most_common()
         tagset = [x[0] for x in tagset_c]
         bin_data = np.array([np.array([1 if t in tagset else 0 for t in x["ingredients"]]) for x in data])
         return bin_data, lbl_data
     
+def shuffledata(datalen, numpart):
+    idx = range(0, datalen)
+    random.shuffle(idx)
+    step = int(datalen/numpart)
+    parts = []
+    for i in range(0,numpart):
+        parts.append(idx[i*step:(i+1)*step])
+    return parts
+    
 def main():
-    processfile('train.json')
+    bin_data, lbl_data = processfile('train.json')
+    indices = shuffledata(len(bin_data), 10)
     
 if __name__ == "__main__":
     main()
